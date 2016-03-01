93e2bd8bf59f94cc4119cf15f6f0590445466909
Brian McHugh <brianomchugh@gmail.com>
2016-02-29 23:36:21 -0500

    fixed up the botManager library and added comments

--- botserver/lib/bot_libraries/botManager.js
+++ botserver/lib/bot_libraries/botManager.js
@@ -1,36 +1,60 @@
+/**
+ * @botManager.js
+ *
+ * This module exports a botManager object. A botManager can:
+ *  -load up active bot details from the database
+ *  -wrap the bot information in a botWrapper object (which sets up
+ *      their logic systems and slack connectivity)
+ *  -start up the bots connection to slack
+ *  -keep track of all running bots in the 'botList' property
+ */
+
+
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/botdb");
-
 var Bot = require('./app/models/bot');
-
 var Botkit = require('botkit');
+
 
 module.exports = function(){
 
-
-    var botInstance = function (botModel){
-        //botModel...not the best name, but this
-        //should just be the instantiated db model
+    /**
+     * botWrapper: a wrapper object to give a bot its logic system and slack connectivity
+     * @param {bot} botModel: unique information about bot, pulled from the database
+     */
+    var botWrapper = function (botModel){
         this.botModel = botModel;
 
         this.botController = Botkit.slackbot({
             debug: false
         });
 
-        this.botInstance = botController.spawn({
+        this.slackBot = botController.spawn({
             token: this.botModel.token
         });
 
-        //TODO get botType from bot.Model.botType
-        this.initHandlers = function(){
-            require('./toneBot.js')(botInstance, botController);
-        };
 
+        /**
+         * start:
+         * -load up the bots logic system (event handlers) by specifying bot type
+         * -start up the connection to the slack api
+         */
         this.start = function(){
-            this.startRTM();
+            //botModel 'type' property must have same name as library file
+            var botType = this.botModel.type;
+            require('./' + botType + '.js')(slackBot, botController);
+
+            this.slackbot.startRTM();
         };
     }
 
+
+    /**
+     * botManager: an object that is responsible for:
+     * -loading up all of the bots from the database
+     * -instantiating and starting up the bots
+     * -keeping a list of running bots in the botList property
+     */
     var botManager = function (){
         this.botList = {
         };
@@ -44,8 +68,7 @@ module.exports = function(){
                 //for each one. create an instance
                 for(var bot in bots){
                     if(bot.active){
-                        var newBotInstance = botInstance(bot);
-                        newBotInstance.initHandlers();
+                        var newBotInstance = botWrapper(bot);
                         newBotInstance.start();
 
                         //add bot to this.botList, with the key being the id
